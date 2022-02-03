1f658afddb1ce4066d187948220aa6fe635bbded
Capdoo <mazher_2014@hotmail.com>
2022-02-03 16:15:29 -0500

    JwtProvider

--- src/main/java/com/example/demo/security/jwt/JwtProvider.java
+++ src/main/java/com/example/demo/security/jwt/JwtProvider.java
@@ -1,5 +1,16 @@
 package com.example.demo.security.jwt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+//Generar token : validación en la creación
+
+@Component
 public class JwtProvider {
 
+	private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class); 
+
+	
+	
 }
--- src/main/java/com/example/demo/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/example/demo/security/jwt/JwtTokenFilter.java
@@ -1,5 +1,6 @@
 package com.example.demo.security.jwt;
 
+//Comprueba por cada petición : Validez f(provider)
 public class JwtTokenFilter {
 
 }
